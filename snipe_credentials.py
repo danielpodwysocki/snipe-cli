5df922ec9b06564f5501d553c7035b4ad7dec55a
Daniel Podwysocki <daniel@contact24.pl>
2021-09-19 21:55:45 +0200

    feat: add handling of a credentials file

---
+++ snipe_credentials.py
@@ -0,0 +1,48 @@
+import yaml
+import os
+from stat import ST_MODE
+import sys
+
+
+def check_perms(path):
+    """
+    returns 1 and prints an error message if the 'others' permission bit
+    on the credentials file is not equal to 0
+    """
+    others_perms = oct(os.stat(os.path.expanduser(path))[ST_MODE])[-1:]
+    try:
+        if int(others_perms) != 0:
+            raise Exception("The 'others' permission bit" +
+                            "on the credentials file is not zero!")
+    except Exception as e:
+        print(e)
+        return 1
+    return 0
+
+
+def get_credentials(path):
+    """
+    takes the path to the credentials as an argument
+    get credentials from a file located in ~/.snipe/credentials.yaml
+    retruns a dict with the following keys: "url" and "api_key"
+    """
+    credentials = {}
+    if check_perms(path):
+        sys.exit(1)
+
+    with open(os.path.expanduser(path), "r") as stream:
+        try:
+            credentials = yaml.safe_load(stream)
+            if "url" not in credentials:
+                raise Exception("the " + path + " file " +
+                                "is missing the url")
+            elif "api_key" not in credentials:
+                raise Exception("the " + path + " file " +
+                                "is missing the api_key")
+            elif credentials["url"][-1] != "/":
+                raise Exception("the url must end with a '/'!")
+        except (yaml.YAMLError, Exception) as e:
+            print(e)
+            sys.exit(1)
+
+    return credentials
