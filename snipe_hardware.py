998aeb94a219054dbb99b165b239fb313b928697
Daniel Podwysocki <daniel@contact24.pl>
2021-09-20 20:37:01 +0200

    add search_hardware and get_hardware_by_tag functions

--- snipe_hardware.py
+++ snipe_hardware.py
@@ -1,8 +1,19 @@
+from requests import api
 from snipeit import Assets
 
-A = Assets()
+assets = Assets()
 
 
 def get_hardware(url, api_key, limit):
-    r = A.get(url, api_key, limit)
+    r = assets.get(url, api_key, limit)
+    return r
+
+
+def search_hardware(url, api_key, limit, search):
+    r = assets.search(url, api_key, limit, keyword=search)
+    return r
+
+
+def get_hardware_by_tag(url, api_key, assset_tag):
+    r = assets.getDetailsByTag(url, api_key, assset_tag)
     return r
